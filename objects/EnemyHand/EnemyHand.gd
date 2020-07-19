10e6f23a30c7dce9d10bcb82202b35f1041889ce
Serhat S <serhats@mail.com>
2020-07-19 23:47:00 +0300

    New hand sprites are added and views are configured for new sprites.

--- objects/EnemyHand/EnemyHand.gd
+++ objects/EnemyHand/EnemyHand.gd
@@ -6,7 +6,7 @@ export var dir: Vector2
 func _enter_tree():
 	randomize()
 	#Set random x pos in 3 lanes
-	var laneWidth = (get_viewport().size.x / 3)
+	var laneWidth = get_viewport_rect().size.x / 3
 	position.x = laneWidth * floor(rand_range(1, 4)) - (laneWidth / 2) 
 	
 	index = floor(rand_range(0,3))
--- objects/Hand.gd
+++ objects/Hand.gd
@@ -2,13 +2,35 @@ extends Area2D
 class_name Hand
 
 export var index: int
+export var skinColor: int
+export var sprite: Texture
 
-var colors: Array = [
-	Color.red,
-	Color.blue,
-	Color.green
+var sprites: Array = [
+	#Rock
+	[
+		preload("res://assets/sprites/hand/rock/Rock.png"),
+		preload("res://assets/sprites/hand/rock/Rock1.png"),
+		preload("res://assets/sprites/hand/rock/Rock2.png"),
+		preload("res://assets/sprites/hand/rock/Rock3.png")
+	],
+	#Paper
+	[
+		preload("res://assets/sprites/hand/paper/Paper.png"),
+		preload("res://assets/sprites/hand/paper/Paper1.png"),
+		preload("res://assets/sprites/hand/paper/Paper2.png"),
+		preload("res://assets/sprites/hand/paper/Paper3.png")
+	],
+	#Scissors
+	[
+		preload("res://assets/sprites/hand/scissors/Scissors.png"),
+		preload("res://assets/sprites/hand/scissors/Scissors1.png"),
+		preload("res://assets/sprites/hand/scissors/Scissors2.png"),
+		preload("res://assets/sprites/hand/scissors/Scissors3.png"),
+	]
 ]
 
+func _enter_tree():
+	skinColor = floor(rand_range(0,4))
+
 func getHand(index: int):
-	#TODO: Get Sprite
-	$Sprite.modulate = colors[index]
+	$Sprite.texture = sprites[index][skinColor]
