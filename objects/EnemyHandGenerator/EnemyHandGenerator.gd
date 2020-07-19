44b36112d4813ad07f254f0dfbe652de21436c0e
Serhat S <serhats@mail.com>
2020-07-19 21:56:10 +0300

    The way generating EnemyHand objects changed.

--- objects/EnemyHandGenerator/EnemyHandGenerator.gd
+++ objects/EnemyHandGenerator/EnemyHandGenerator.gd
@@ -2,17 +2,52 @@ extends Node
 
 var timer:Timer
 var enemyHand = preload("res://objects/EnemyHand/EnemyHand.tscn")
+export var speed: float
+export var interval: float
+export var speedIncrement: float
+var enemyHandCount: int
+var incrementionCounter: float
 
 func _enter_tree():
-	timer = createTimer(2)
+	timer = createTimer()
 	timer.connect("timeout", self, "onTimeout")
 
-func createTimer(var time: float) -> Timer:
+func createTimer(var time: float = 1) -> Timer:
 	var timer = Timer.new()
 	add_child(timer);
+	timer.one_shot = true
 	timer.start(time)
 	return timer
 
 func onTimeout():
-	#Create Enemy Hand object instance
-	add_child(enemyHand.instance())
+	createEnemyHandInstance()
+	enemyHandCount += 1
+	
+	setNewSpeedValues()
+	logValues()
+	
+	timer.start(interval / speed)
+
+func createEnemyHandInstance():
+	var enemyHandInstance = enemyHand.instance()
+	enemyHandInstance.speed = speed
+	add_child(enemyHandInstance)
+
+func setNewSpeedValues():
+	speed += speedIncrement
+	incrementionCounter += speedIncrement
+	speedIncrement -= speedIncrement / 200
+	
+	#Each 50 speed incrementation decreases interval by 1%
+	if(incrementionCounter > 50):
+		interval -= interval / 100
+		incrementionCounter = 0
+
+func logValues():
+	print(
+	self.name, ": ",
+	"\t Created: ", enemyHandCount,
+	"\t Speed: ", speed,
+	"\t Interval: ", interval,
+	 "\t Time: ", interval/speed,
+	 "\t SIncrement: ", speedIncrement)
