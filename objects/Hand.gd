230f02aa4bdfc8e52bc1082e9037853dd5afeb9e
Serhat S <serhats@mail.com>
2020-07-19 11:38:24 +0300

    Hand object type changed to Area2D, so no extra Area2D child needed.

--- objects/Hand.gd
+++ objects/Hand.gd
@@ -1,4 +1,4 @@
-extends Node2D
+extends Area2D
 class_name Hand
 
 export var index: int
--- objects/PlayerHand/MouseInput.gd
+++ objects/PlayerHand/MouseInput.gd
@@ -2,7 +2,7 @@ extends Node
 
 signal changeHand
 
-func _on_input_event(viewport, event, shape_idx):
+func _on_PlayerHand_input_event(viewport, event, shape_idx):
 	if event is InputEventMouseButton:
 		if event.is_pressed():
 			emit_signal("changeHand")
