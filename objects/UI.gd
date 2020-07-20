f77891700bbb5627d300a7d8d72380fc9bd3a477
Serhat S <serhats@mail.com>
2020-07-20 16:49:09 +0300

    StartMenu UI is added.

--- objects/UI.gd
+++ objects/UI.gd
@@ -3,11 +3,27 @@ extends Control
 var game: Node2D
 var backgroundColorIncrement: float = 0.002
 
+signal gameStart
+signal okayToEndGame
+
 func _enter_tree():
 	game = self.owner
 	game.connect("score", self, "onScore")
+	game.connect("gameOver", self, "onGameOver")
 
 
 func onScore():
 	$Background.color.h += backgroundColorIncrement
 	print(self.name, ": ", "Background Color: ", $Background.color)
+
+
+func _on_PlayButton_pressed():
+	$Background/Fade/AnimationPlayer.play_backwards("Fade")
+	$StartGameMenu.queue_free()
+	emit_signal("gameStart")
+
+func onGameOver():
+	print(self.name, ": Begin")
+	$Background/Fade/AnimationPlayer.play("Fade")
+	yield($Background/Fade/AnimationPlayer, "animation_finished")
+	emit_signal("okayToEndGame")
--- scenes/Game.gd
+++ scenes/Game.gd
@@ -2,6 +2,7 @@ extends Node2D
 
 var score: int
 signal score
+signal gameOver
 
 func _enter_tree():
 	$PlayerHand.connect("gameOver", self, "onGameOver")
@@ -12,10 +13,21 @@ func _enter_tree():
 	$PlayerHand2.connect("score", self, "onScore")
 	$PlayerHand3.connect("score", self, "onScore")
 
+	$UI.connect("gameStart", self, "onGameStart")
+	get_tree().paused = true
+
 func onGameOver():
+	get_tree().paused = true
+	emit_signal("gameOver")
+	yield($UI, "okayToEndGame")
 	get_tree().reload_current_scene()
+
 
 func onScore():
 	score += 1
 	print(self.name, ": ", "Score: ", score)
 	emit_signal("score")
+
+func onGameStart():
+	print(self.name, ": ", "Game Started!")
+	get_tree().paused = false
